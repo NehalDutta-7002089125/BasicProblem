568ab3b849ab46ed42f9131624a66661fefe6280
NehalDutta-7002089125 <nihal326dutta@gmail.com>
2022-12-01 16:10:20 +0530

    Nehal

--- Program.cs
+++ Program.cs
@@ -1,49 +1,21 @@
 ﻿using System;
 
-namespace UC_1_Flipcoin
+namespace UC_2_Leapyear
 {
-    internal class Program
+    internal class Leapyear
     {
         static void Main(string[] args)
         {
-            int HC = 0;
+            Console.WriteLine("Enter the year");
+            int year = Convert.ToInt32(Console.ReadLine());
 
-            int TC = 0;
-
-            double Hper = 0;
-
-            double Tper = 0;
-
-            Console.WriteLine("Enter the number of flip");
-
-            int Flips = Convert.ToInt32(Console.ReadLine());
-
-            if (Flips > 0)
+            if ((year%4 == 0) && (year%100!= 0) || (year%400 == 0))
             {
-                for (int i = 0; i < Flips; i++)
-                {
-                    Random random = new Random();
-
-                    int number = random.Next(2);
-
-                    Console.WriteLine(number);
-
-                    if (number == 0)
-                    {
-                        TC++;
-                    }
-                    else
-                    {
-                        HC++;
-                    }
-                }
-                Hper = HC * 100 / Flips;
-
-                Tper = TC * 100 / Flips;
-
-                Console.WriteLine("headpercentage" + Tper);
-
-                Console.WriteLine("tailpercentage" + Hper);
+                Console.WriteLine(year +" is a leap year ");
+            }
+            else
+            {
+                Console.WriteLine(year + "is not a leap year");
             }
         }
     }
